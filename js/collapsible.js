83c73e0f016da243dfc8254a5f9bfba66d82de07
BredeBorresen <bredee02@gmail.com>
2024-10-22 11:57:19 +0200

    Nesten ferdig stilt prosjekt, ta utgangspunkt i dette ved utvikling, må lage ny github for å vise samarbeid bedre

---
+++ js/collapsible.js
@@ -0,0 +1,15 @@
+// Collapsible script - Hentet fra W3Schools, url: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_collapsible_symbol
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
